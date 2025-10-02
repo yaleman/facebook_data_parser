e86cc72d60dc256da15c00274b2cd140f86795b2
James Hodgkinson <james@terminaloutcomes.com>
2025-10-03 00:02:58 +1000

    chore: cleanup

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,22 @@ fn main() {
 
     folder_checks();
 
-    // eprintln!("CliOpts: {:?}", cliopts);
-
     match cliopts.command {
         CliCommands::Activity { command } => match command {
-            ActivityActivity::Messages(msg) => {
-                match msg.command {
-                    ActivityMessagesSubCommand::ReorgImages => {
-                        reorg_images(msg).expect("Failed to reorg messages");
-                    }
-                    ActivityMessagesSubCommand::ReorgVideos => {
-                        reorg_videos(msg).expect("Failed to reorg videos");
-                    }
-                    ActivityMessagesSubCommand::ListFiles => {
-                        list_files(msg).expect("Failed to list files")
-                    }
-                    ActivityMessagesSubCommand::SearchMessages { path } => {
-                        search_messages(path).expect("Failed to search messages")
-                    }
+            ActivityActivity::Messages(msg) => match msg.command {
+                ActivityMessagesSubCommand::ReorgImages => {
+                    reorg_images(msg).expect("Failed to reorg messages");
                 }
-                // reorg_images(msg).expect("Failed to reorg messages");
-            }
+                ActivityMessagesSubCommand::ReorgVideos => {
+                    reorg_videos(msg).expect("Failed to reorg videos");
+                }
+                ActivityMessagesSubCommand::ListFiles => {
+                    list_files(msg).expect("Failed to list files")
+                }
+                ActivityMessagesSubCommand::SearchMessages { path } => {
+                    search_messages(path).expect("Failed to search messages")
+                }
+            },
         },
     }
 }
